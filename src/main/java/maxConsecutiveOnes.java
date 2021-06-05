7fbe5ab226776331a0ece20347e20dd52a278236
clive <tsunguc@cardiff.ac.uk>
2021-06-05 20:25:59 +0100

    Max consecutive 1s in binary

---
+++ src/main/java/maxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+public class maxConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int sum = 0;
+        int max = 0;
+        for(int i = 0 ; i<nums.length ; i++ ){
+
+            if(nums[i] == 1 ){
+                sum = sum + 1;
+                if(sum > max){
+                    max = sum;
+                }
+            }
+            else{
+                sum = 0;
+            }
+        }
+        return max;
+
+    }
+
+}
